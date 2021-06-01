c18f5ce530b5cba130aa536a287e8412591b7690
Mariojj <mariojj@live.com>
2021-06-01 12:24:33 +0200

    añadiendo otra dependencia en archivo pom y anotación en equipoRepository

--- liga/src/main/java/com/sw2/liga/EquipoRepository.java
+++ liga/src/main/java/com/sw2/liga/EquipoRepository.java
@@ -2,7 +2,9 @@ package com.sw2.liga;
 
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "equipos")
 interface EquipoRepository extends CrudRepository<Equipo, Long> {
 
 }
