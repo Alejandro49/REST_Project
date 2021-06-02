1b6888babd6ffdee5160ff947bed8f179a8ac340
Mariojj <mariojj@live.com>
2021-06-02 14:34:32 +0200

    añadiendo hascode y ToString que faltaban

--- liga/src/main/java/com/sw2/liga/Equipo.java
+++ liga/src/main/java/com/sw2/liga/Equipo.java
@@ -40,37 +40,6 @@ public class Equipo {
 	}
 	
 	
-/*	 @Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Equipo other = (Equipo) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-*/
-	@Override
-	  public String toString() {
-	    return "Equipo{" + "id=" + this.id + ", nombre='" + this.nombre + '\'' + ", entrenador='" + this.entrenador + '\'' + " titulos: " + this.titulos + "}";
-	  }
-	
-	
 	public Long getId() {
 		return id;
 	}
@@ -105,5 +74,33 @@ public class Equipo {
 	}
 	
 	
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Equipo other = (Equipo) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	  public String toString() {
+	    return "Equipo{" + "id=" + this.id + ", nombre='" + this.nombre + '\'' + ", entrenador='" + this.entrenador + '\'' + " titulos: " + this.titulos + "}";
+	  }	
 
 }
